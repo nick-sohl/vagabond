b07058d01ebb8de72a70254bd7bc1d9baa9196e6
Nick Sohl <nicksohl59@gmail.com>
2026-03-20 17:18:56 +0100

    update public entry point with routing and autoloader setup

--- public/index.php
+++ public/index.php
@@ -1,30 +1,42 @@
 <?php
 
+use FlightBookingSystem\Application\Service\AirportService;
+use FlightBookingSystem\Application\Service\FlightService;
+use FlightBookingSystem\Infrastructure\Repository\SqliteAirportRepository;
+use FlightBookingSystem\Infrastructure\Repository\SqliteFlightRepository;
+use FlightBookingSystem\Presentation\Controller\AirportController;
+use FlightBookingSystem\Presentation\Controller\BookingController;
+use FlightBookingSystem\Presentation\Controller\FlightController;
+use FlightBookingSystem\Presentation\Controller\HomeController;
+use FlightBookingSystem\Presentation\Router\Router;
+
+// Composer autoload -> Autoload all Classes
 require __DIR__ . '/../vendor/autoload.php';
 
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// 1. Build Adapters
+$sqliteFlightRepo = new SqliteFlightRepository();
+$sqliteAirportRepo = new SqliteAirportRepository();
 
-// TODO: Bootstrap dependencies
-$repo;
-$service;
-$controller;
+// 2. Build Services and inject Adapters
+$flightService = new FlightService($sqliteFlightRepo);
+$airportService = new AirportService($sqliteAirportRepo);
 
-// match($path) {
-//   '/flights' =>
-// }
-?>
+// 3. Build Controller and inject Services
+$homeController = new HomeController();
+$flightController = new FlightController($flightService);
+$bookingsController = new BookingController();
+$airportController = new AirportController($airportService);
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Flight Booking System</title>
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <h1>Hello World!</h1>
-    <script src="index.js"></script>
-  </body>
-</html>
+// 4. Register routes with instances
+$router = new Router();
+
+// Page endpoints (render full views)
+$router->get('/', [$homeController, 'index']);
+$router->get('/flights', [$flightController, 'index']);
+$router->get('/bookings', [$bookingsController, 'index']);
+
+// Resource endpoints (render fragments for HTMX components)
+$router->get('/api/airports', [$airportController, 'airports']);
+
+// Fetch method and path from request and invoke dispatch method of Router class
+$router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
